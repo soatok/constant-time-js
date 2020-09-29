005a6049ab635376b8ee2a3ff6c09e55ed8db3d5
Soatok Dreamseeker <soatok.dhole@gmail.com>
2020-09-29 16:17:41 -0400

    Create/use int32 class wrapping Uint16Array.
    This allows us to safely compute on platforms that have weird timing issues with the most significant bit (MSB).

--- lib/compare.ts
+++ lib/compare.ts
@@ -1,3 +1,4 @@
+import { int32 } from './int32';
 
 export function compare(left: Uint8Array, right: Uint8Array): number {
     if (left.length !== right.length) {
@@ -6,16 +7,30 @@ export function compare(left: Uint8Array, right: Uint8Array): number {
     let gt: number = 0;
     let eq: number = 1;
     let i: number = left.length;
+    let l: int32;
+    let r: int32;
     while (i > 0) {
         i--;
-        gt |= ((right[i] - left[i]) >>> 8) & eq;
-        eq &= ((right[i] ^ left[i]) - 1) >>> 8;
+        r = int32.fromInt(right[i]);
+        l = int32.fromInt(left[i]);
+        gt |= r.sub(l).msb();
+        eq &= r.xor(l).sub(1).msb();
     }
+    l.wipe();
+    r.wipe();
     return (gt + gt + eq) - 1;
 }
 
 export function compare_ints(left: number, right: number): number {
+    /*
     let gt: number = (right - left) >>> 31;
     let eq: number = ((right ^ left) - 1) >>> 31;
+    */
+    const L: int32 = int32.fromInt(left);
+    const R: int32 = int32.fromInt(right);
+    const gt: number = R.sub(L).msb();
+    const eq: number = R.xor(L).sub(1).msb();
+    L.wipe();
+    R.wipe();
     return (gt + gt + eq) - 1;
 }
---
+++ lib/int32.ts
@@ -0,0 +1,198 @@
+export class int32 {
+    private limbs: Uint16Array;
+
+    constructor(low: number, high: number) {
+        this.limbs = new Uint16Array([low, high]);
+    }
+
+    static zero(): int32 {
+        return new int32(0, 0);
+    }
+
+    static one(): int32 {
+        return new int32(1, 0);
+    }
+
+    static fromInt(int: number): int32 {
+        return new int32(int & 0xffff, int >>> 16);
+    }
+
+    low() {
+        return this.limbs[0];
+    }
+
+    high() {
+        return this.limbs[1];
+    }
+
+    lsb() {
+        return this.limbs[0] & 1;
+    }
+
+    msb() {
+        return (this.limbs[1] >>> 15) & 1;
+    }
+
+    add(arg: number|int32): int32 {
+        if (arg instanceof int32) {
+            return int32_add_int32(this, arg);
+        }
+        return int32_add_number(this, arg);
+    }
+
+    and(arg: number|int32): int32 {
+        if (arg instanceof int32) {
+            return int32_and_int32(this, arg);
+        }
+        return int32_and_number(this, arg);
+    }
+
+    compare(arg: number|int32): int32 {
+        if (typeof arg === 'number') {
+            arg = int32.fromInt(arg);
+        }
+        return int32_compare(this, arg);
+    }
+
+    lshift(amount: number): int32 {
+        if (amount > 31) {
+            return int32.zero();
+        }
+        return int32_lshift(this, amount);
+    }
+
+    negate(): int32 {
+        return int32_negate(this);
+    }
+
+    or(arg: number|int32): int32 {
+        if (arg instanceof int32) {
+            return int32_or_int32(this, arg);
+        }
+        return int32_or_number(this, arg);
+    }
+
+    rshift(amount: number): int32 {
+        if (amount > 31) {
+            return int32.zero();
+        }
+        return int32_rshift(this, amount);
+    }
+
+    sub(arg: number|int32): int32 {
+        if (arg instanceof int32) {
+            return int32_sub_int32(this, arg);
+        }
+        return int32_sub_number(this, arg);
+    }
+
+    xor(arg: number|int32): int32 {
+        if (arg instanceof int32) {
+            return int32_xor_int32(this, arg);
+        }
+        return int32_xor_number(this, arg);
+    }
+
+    toHex(): string {
+        const l: string = (this.low() & 0xffff)
+            .toString(16)
+            .padStart(4, '0');
+        const h: string = (this.high() & 0xffff)
+            .toString(16)
+            .padStart(4, '0');
+        return h.concat(l);
+    }
+
+    toNumber(): number {
+        return (this.low() | (this.high() << 16));
+    }
+
+    wipe(): void {
+        this.limbs[0] ^= this.limbs[0];
+        this.limbs[1] ^= this.limbs[1];
+    }
+}
+
+function int32_add_int32(a: int32, b: int32): int32 {
+    let l: number = a.low() + b.low();
+    let h: number = a.high() + b.high() + (l >>> 16);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_add_number(a: int32, b: number): int32 {
+    let l: number = a.low() + (b & 0xffff);
+    let h: number = a.high() + (b >>> 16) + (l >>> 16);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_and_int32(a: int32, b: int32): int32 {
+    let l: number = a.low() & b.low();
+    let h: number = a.high() & b.high();
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_and_number(a: int32, b: number): int32 {
+    let l: number = a.low() & (b & 0xffff);
+    let h: number = a.high() & (b >>> 16);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_compare(left, right): int32 {
+    let gt: number = right.sub(left).msb();
+    let eq: number = right.xor(left).sub(1).msb();
+    return int32.fromInt((gt + gt + eq) - 1);
+}
+
+function int32_lshift(a: int32, x: number) {
+    let l: number = (a.low() << x);
+    let h: number = (l >>> 16) | (a.high() << x);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_negate(a: int32): int32 {
+    let l: number = a.low() ^ 0xffff;
+    let h: number = a.high() ^ 0xffff;
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_or_int32(a: int32, b: int32): int32 {
+    let l: number = a.low() | b.low();
+    let h: number = a.high() | b.high();
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_or_number(a: int32, b: number): int32 {
+    let l: number = a.low() | (b & 0xffff);
+    let h: number = a.high() | (b >>> 16);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_rshift(a: int32, amount: number) {
+    let m: number = (1 << (amount + 1)) - 1;
+    let h: number = (a.high() >>> amount);
+    let l: number = (a.low() >>> amount) | (((a.high() & m) << 16) >>> amount);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_sub_int32(a: int32, b: int32): int32 {
+    let l: number = a.low() - b.low();
+    let h: number = a.high() - b.high() + (l >>> 16);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+function int32_sub_number(a: int32, b: number): int32 {
+    let l: number = a.low() - (b & 0xffff);
+    let h: number = a.high() - (b >>> 16) + (l >>> 16);
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_xor_int32(a: int32, b: int32): int32 {
+    let l: number = a.low() ^ b.low();
+    let h: number = a.high() ^ b.high();
+    return new int32(l & 0xffff, h & 0xffff);
+}
+
+function int32_xor_number(a: int32, b: number): int32 {
+    let l: number = a.low() ^ (b & 0xffff);
+    let h: number = a.high() ^ (b >>> 16);
+    return new int32(l & 0xffff, h & 0xffff);
+}
