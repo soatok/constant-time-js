190a7d498433e0baba251170775aa1d74cb49963
Adam Ochayon <adam.ochayon@gmail.com>
2020-10-01 18:52:24 -0400

    Fix: Standardize quotes
    Standardizes the use of single quotes in imports

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-export { compare, compare_ints } from "./lib/compare";
-export { equals, hmac_equals } from "./lib/equals";
+export { compare, compare_ints } from './lib/compare';
+export { equals, hmac_equals } from './lib/equals';
 export { intdiv, modulo } from './lib/intdiv';
 export { resize } from './lib/resize';
 export { select_ints, select } from './lib/select';
